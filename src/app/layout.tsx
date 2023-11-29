52af4c5bc9789504bdad75264d1714ed0af41c88
Anh Dinh <dlna85.acc@gmail.com>
2023-11-29 18:40:08 +0700

    chore: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,60 @@
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 import type { Metadata } from 'next';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
+const googleSans = localFont({
+    src: [
+        {
+            path: '../../public/fonts/GoogleSans-Thin.ttf',
+            weight: '100',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/GoogleSans-Light.ttf',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/GoogleSans-Regular.ttf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/GoogleSans-Medium.ttf',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/GoogleSans-Bold.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/GoogleSans-Black.ttf',
+            weight: '900',
+            style: 'normal',
+        },
+    ],
+});
 
 export const metadata: Metadata = {
-  title: 'Fessior Tools',
-  description: 'Utility tools developed by Fessior Community',
+    title: 'Fessior Tools',
+    description: 'Utility tools developed by Fessior Community',
 };
 
 type RootLayoutProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 export default function RootLayout(props: RootLayoutProps) {
-  const { children } = props;
+    const { children } = props;
 
-  return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={googleSans.className}>{children}</body>
+        </html>
+    );
 }
