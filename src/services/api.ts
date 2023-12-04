7efed6299e4ff9e62fb1feabcde3382eb2f52073
Anh Dinh <dlna85.acc@gmail.com>
2023-12-04 15:40:34 +0700

    chore: update env variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 instance.interceptors.request.use(
   (config) => {
-    config.headers.Authorization =
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiNjU1YTQyYzFmY2RkNmM3MTZlYmM4NjJlIiwiaWF0IjoxNzAwNDE0MTQ1LCJleHAiOjE3MzE5NTAxNDV9.E_ZNREzb8XFJ17rqG_Mteu6GmxMEYzTPTVcVu7DdOYc';
+    config.headers.Authorization = `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`;
     return config;
   },
   (error) => {
