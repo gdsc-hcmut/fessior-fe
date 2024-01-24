fd14292a2c6a3016eaafe2edd8e946c1ed0f458e
kahn-29 <nmqkhanh2005@gmail.com>
2024-01-24 16:54:58 +0700

    feat: qr customize navbar

---
+++ src/app/qrcode/sub-nav.tsx
@@ -0,0 +1,12 @@
+export interface titleProps {
+  content: string;
+}
+export const Title: React.FC<titleProps> = ({ content }) => {
+  return (
+    <button className='rounded-lg hover:bg-[#0B2878] hover:text-[white]'>
+      <div className='inline-flex items-center rounded-lg border border-black px-[8px] py-[4px] text-[14px] md:px-[32px] md:py-[8px] md:text-[18px]'>
+        <p className='text-center'>{content}</p>
+      </div>
+    </button>
+  );
+};
