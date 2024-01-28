610b03eda8fa1dded9cb6a10bb452359ff3eb5d5
Anh Dinh <dlna85.acc@gmail.com>
2024-01-28 23:56:42 +0700

    chore: revert wave.css to index.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import HomeProjectCarousel from '@/components/home/home-project-carousel';
 import HomeTools from '@/components/home/home-tools';
 import useScreenSize from '@/hooks/useScreenSize';
 import { projects } from '@/services/project.service';
-import '@/styles/wave.css';
+import '@/styles/index.css';
 
 export default function HomePage() {
   const { screenSize, loaded } = useScreenSize();
