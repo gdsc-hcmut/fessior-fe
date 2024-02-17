4cf7c1c77e343ceb70a3fedeb92c78091b3ece11
NooBat <khoi27012003@gmail.com>
2024-02-17 22:46:46 +0700

    chore: improve assets structure
    - add a script to automatically add images, svgs to assets folder and
    create index.ts to export them as objects.

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export const images = {
+  Banner: require('./img/banner.jpg'),
+  Fessior: require('./img/fessior.jpg'),
+};
+
+export const svgs = {
+  AdminIcon: require('./svg/admin-icon.svg'),
+};
