3aaa3adcca3b113b247efda14c47d8ddeaf1ce0c
Anh Dinh <dlna85.acc@gmail.com>
2024-04-09 23:16:58 +0700

    chore: add src/data to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/services/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/data/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [require('tailwind-scrollbar-hide')],
 };
